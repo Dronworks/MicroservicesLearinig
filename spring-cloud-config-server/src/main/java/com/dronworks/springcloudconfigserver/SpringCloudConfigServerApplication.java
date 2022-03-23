9a7c63493bf77ab6dc5c2769f0f8840a5aa72d6e
Andrey Dobrikov <andrejy601@gmail.com>
2022-03-23 17:50:11 +0200

    adding config server

---
+++ spring-cloud-config-server/src/main/java/com/dronworks/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.dronworks.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer // For accessing the link - localhost:8888/limits-service/default (the file data)
+@SpringBootApplication
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+
+}
