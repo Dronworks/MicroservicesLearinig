b28c34a237e7df346748b62bfc44f4653179813e
Andrey Dobrikov <andrejy601@gmail.com>
2022-03-24 09:21:39 +0200

    filled database and created currency conversion service

--- currency-exchange-service/src/main/java/com/dronworks/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/dronworks/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,6 +1,7 @@
 package com.dronworks.currencyexchangeservice.controller;
 
 import com.dronworks.currencyexchangeservice.entity.CurrencyExchange;
+import com.dronworks.currencyexchangeservice.repository.CurrencyExchangeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,16 +9,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
 public class CurrencyExchangeController {
+
+    @Autowired
+    private CurrencyExchangeRepository currencyExchangeRepository;
 
     @Autowired
     private Environment environment;
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(65));
+//        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(65));
+        CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
+        if(currencyExchange == null) {
+            throw new RuntimeException(String.format("Cant find exchange from %s to %s", from, to));
+        }
         currencyExchange.setEnvironment(environment.getProperty("local.server.port"));
         return currencyExchange;
     }
---
+++ currency-exchange-service/src/main/java/com/dronworks/currencyexchangeservice/repository/CurrencyExchangeRepository.java
@@ -0,0 +1,10 @@
+package com.dronworks.currencyexchangeservice.repository;
+
+import com.dronworks.currencyexchangeservice.entity.CurrencyExchange;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
+
+    CurrencyExchange findByFromAndTo(String from, String to);
+
+}
