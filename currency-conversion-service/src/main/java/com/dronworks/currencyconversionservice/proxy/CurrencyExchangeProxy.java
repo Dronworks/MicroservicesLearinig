170054f994fbcfcfac1f22f611cce7b99476b986
Andrey Dobrikov <andrejy601@gmail.com>
2022-03-25 10:59:39 +0300

    proxy via eureka

--- currency-conversion-service/src/main/java/com/dronworks/currencyconversionservice/proxy/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/dronworks/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -5,7 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "currency-exchange", url = "localhost:8000")
+// Fixed @FeignClient(value = "currency-exchange", url = "localhost:8000")
+// Eureka:
+@FeignClient(value = "currency-exchange")
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
