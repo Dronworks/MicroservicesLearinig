20ba19a7866e9b761bcddbc7182fe3a7f823bcf3
Andrey Dobrikov <andrejy601@gmail.com>
2022-03-25 18:36:30 +0300

    Manual redirect with configuration

--- api-gateway/src/main/java/com/dronworks/apigateway/configurations/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/dronworks/apigateway/configurations/ApiGatewayConfiguration.java
@@ -1,14 +1,9 @@
 package com.dronworks.apigateway.configurations;
 
-import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.Buildable;
-import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.function.Function;
 
 @Configuration
 public class ApiGatewayConfiguration {
@@ -20,6 +15,16 @@ public class ApiGatewayConfiguration {
                 .filters(f -> f.addRequestHeader("MyHeader", "MyUri")
                         .addRequestParameter("SomeParam", "Param"))
                 .uri("http://httpbin.org:80"))
+                .route(p -> p.path("/currency-exchange/**")
+                        .uri("lb://CURRENCY-EXCHANGE")) // Load Balance - gets from EUREKA
+                .route(p -> p.path("/currency-conversion/**")
+                        .uri("lb://CURRENCY-CONVERSION")) // Load Balance - gets from EUREKA
+                .route(p -> p.path("/currency-conversion-feign/**")
+                        .uri("lb://CURRENCY-CONVERSION")) // Load Balance - gets from EUREKA
+                .route(p -> p.path("/currency-conversion-new/**")
+                        .filters(f -> f.rewritePath("/currency-conversion-new/(?<segment>.*)",
+                                "/currency-conversion-feign/${segment}")) // Rewrite path with the segment that comes after
+                        .uri("lb://CURRENCY-CONVERSION"))
                 .build();
     }
 
