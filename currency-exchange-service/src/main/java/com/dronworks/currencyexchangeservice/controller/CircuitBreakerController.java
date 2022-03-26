ee661c2f5cad5cc3cfb2935eaec7cd0f2aefc04a
Andrey Dobrikov <andrejy601@gmail.com>
2022-03-26 07:49:11 +0300

    Default retry (3 times)

---
+++ currency-exchange-service/src/main/java/com/dronworks/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -0,0 +1,25 @@
+package com.dronworks.currencyexchangeservice.controller;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CircuitBreakerController {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
+
+    @GetMapping("/sample-api")
+    @Retry(name="default")
+    public String sampleApi() {
+        logger.info("Sample api call received");
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy", String.class);
+
+        return forEntity.getBody();
+    }
+
+}
