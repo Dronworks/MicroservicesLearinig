9e89e23b5a0bcee5287ae70d3691dc77e6c458b8
Andrey Dobrikov <andrejy601@gmail.com>
2022-03-26 08:59:20 +0300

    circuitbreaker - lets some requests and after time returns fallback and few retries

--- currency-exchange-service/src/main/java/com/dronworks/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/dronworks/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,5 +1,6 @@
 package com.dronworks.currencyexchangeservice.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,7 +15,17 @@ public class CircuitBreakerController {
     Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
 
     @GetMapping("/sample-api")
-    @Retry(name="sample-api-prop", fallbackMethod = "hardcodedResponse")
+//    @Retry(name="sample-api-prop", fallbackMethod = "hardcodedResponse")
+    /*
+        Starts with close state - allowing sending much of the requests.
+        After a threshold open state and stopping letting the requests, returning fallback.
+        After another threshold half-open state - each few seconds lets few requests to proceed. If success close state.
+        If not open state.
+        resillience4j.readme.io/docs/circuitbreaker - shows more options. For example threshold for long requests(configurable)
+        can see configuration example in Getting Started on the left.
+        Threshold in percents
+     */
+    @CircuitBreaker(name="default", fallbackMethod = "hardcodedResponse")
     public String sampleApi() {
         logger.info("Sample api call received");
         ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy", String.class);
