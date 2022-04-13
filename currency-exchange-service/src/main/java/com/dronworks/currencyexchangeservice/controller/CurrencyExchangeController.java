0a0853486778fc38fe029d885e6d3eaa4af3548f
Andrey Dobrikov <andrejy601@gmail.com>
2022-04-13 19:33:26 +0300

    Connecting with zipkin

--- currency-exchange-service/src/main/java/com/dronworks/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/dronworks/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -3,17 +3,18 @@ package com.dronworks.currencyexchangeservice.controller;
 import com.dronworks.currencyexchangeservice.entity.CurrencyExchange;
 import com.dronworks.currencyexchangeservice.exception.ExchangeRatesNotFoundException;
 import com.dronworks.currencyexchangeservice.repository.CurrencyExchangeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-import java.util.List;
-
 @RestController
 public class CurrencyExchangeController {
+
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class.getSimpleName());
 
     @Autowired
     private CurrencyExchangeRepository currencyExchangeRepository;
@@ -24,6 +25,7 @@ public class CurrencyExchangeController {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
 //        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(65));
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
         CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
         if(currencyExchange == null) {
             throw new ExchangeRatesNotFoundException(String.format("Cant find exchange from %s to %s", from, to));
